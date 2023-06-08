bd979cc144b2c6eaf5aea7dad32e2c9548334048
lorossi <mail@lorenzoros.si>
2023-06-08 15:10:13 +0200

    updated methods

--- modules/links.py
+++ modules/links.py
@@ -10,7 +10,7 @@ class Link:
         display_name: str,
         name: str,
         lan_ip: str,
-        zerotier_ip: str,
+        ip: str,
         port: str,
         path: str = None,
     ) -> Link:
@@ -20,7 +20,7 @@ class Link:
             display_name (str): displayed name of the service
             name (str): short name of the service
             lan_ip (str): ip of the service on the local network
-            zerotier_ip (str): ip of the service on the ZeroTier network
+            ip (str): ip of the service on the network
             port (str): port of the service
             path (str, optional): url path of the service. Defaults to None.
 
@@ -30,54 +30,55 @@ class Link:
         self._display_name = display_name
         self._name = name
         self._lan_ip = lan_ip
-        self._zerotier_ip = zerotier_ip
+        self._ip = ip
         self._port = port
         self._path = path
 
     @staticmethod
-    def fromJSON(json: dict[str, str]) -> Link:
-        """Create a Link object from a json dictionary."""
-        return Link(
-            json["display_name"],
-            json["name"],
-            json["lan_ip"],
-            json["zerotier_ip"],
-            json["port"],
-            json.get("path", None),
-        )
+    def fromDict(dictionary: dict) -> Link:
+        """Create a Link object from a dictionary.
+
+        Args:
+            dictionary (dict): The dictionary to create the Link object from.
+
+        Returns:
+            Link: The Link object.
+        """
+        return Link(**dictionary)
 
     def getFullUrl(self, zerotier: bool = False) -> str:
         """Get the full url of the service.
 
         Args:
-            zerotier (bool, optional): True if the service is accessed \
+            zerotier (bool, optional): True if the service is accessed gradients_path
                 through the ZeroTier network. Defaults to False.
 
         Returns:
             str
         """
         if zerotier:
-            return self.zerotier_url
+            return self.url
 
         return self.lan_url
 
-    def getPropertiesDict(self, zerotier: bool = False) -> dict[str, str]:
-        """Get the properties of the service as a dictionary, \
-            to be used by the Flask renderer.
+    def getPropertiesDict(self, lan: bool = False) -> dict[str, str]:
+        """Get the properties of the service as a dictionary, gradients_path
+            to be used by the HTTP renderer.
+
 
         Args:
-            zerotier (bool, optional): True if the service is accessed \
-                through the ZeroTier network. Defaults to False.
+            lan (bool, optional): True if the service is accessed through the
+                local network. Defaults to False.
 
         Returns:
-            dict[str, str
+            dict[str, str]
         """
         properties = {"name": self._display_name}
 
-        if zerotier:
-            properties["href"] = self.zerotier_url
-        else:
+        if lan:
             properties["href"] = self.lan_url
+        else:
+            properties["href"] = self.url
 
         return properties
 
@@ -90,9 +91,9 @@ class Link:
         return f"http://{self._lan_ip}:{self._port}/{self._path}"
 
     @property
-    def zerotier_url(self) -> str:
-        """Get the url of the service on the ZeroTier network."""
+    def url(self) -> str:
+        """Get the url of the service on the network."""
         if self._path is None:
-            return f"http://{self._zerotier_ip}:{self._port}"
+            return f"http://{self._ip}:{self._port}"
 
-        return f"http://{self._zerotier_ip}:{self._port}/{self._path}"
+        return f"http://{self._ip}:{self._port}/{self._path}"
