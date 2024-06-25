bb544201872ee7d814ce102666412e08316f6024
Lorenzo Rossi <mail@lorenzoros.si>
2024-06-25 14:39:02 +0200

    added script killer

---
+++ killer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# stop the script
+for file in *.py
+do
+    # Get the PID of the process
+    pid=$(pgrep -f $file)
+    if [ ! -z $pid ]
+    then
+        # Kill the process
+        echo -e "has PID: $pid. Killing it..."
+        kill $pid
+    else
+        # If the process is not running, print a message
+        echo -e "file is not running."
+    fi
+done
